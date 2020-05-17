c4e8a73da9b398ce7bd9c666bf67aa8771efaf97
Aretas Paulauskas <aretas.pau@gmail.com>
2020-05-17 21:17:26 +0300

    create testing files when starting hades server

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+testMac=00:00:00:00:00:00
+
+# create files for testing
+touch models/"${testMac}"
+cat > states/"${testMac}" << EOL
+{"stats": {"prev_temperature": 25.2, "prev_delta": 0.5, "curr_temperature": 25.2, "send_interval": 2.0}}
+EOL
+
 python hades.py --enable_select_tf_ops
